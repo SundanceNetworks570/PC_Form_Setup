922e329c9611d347bd682d5463ece8f168223112
SundanceNetworks570 <support@sundancenetworks.com>
2026-01-23 15:48:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -110,8 +110,7 @@ app.post('/phone-slip', async (req, res) => {
 
     await transporter.sendMail({
       from: process.env.FROM_EMAIL || process.env.SMTP_USER,
-      to: 'support@sundancenetworks.com',                   // add more recipients if desired
-      cc: 'the20group@sundancenetworks.com'
+      to: 'support@sundancenetworks.com',           // add more recipients if desired
       subject: `New Phone Slip${data.urgency ? ' - ' + data.urgency : ''}`,
       text: textBody,
     });
